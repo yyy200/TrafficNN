121e99ce2cf22f8ea7f846c70f84da0a0a568906
Youssef El Mays <yelmays@icloud.com>
2021-03-16 17:08:04 -0600

    heartbeat

--- traffic.py
+++ traffic.py
@@ -9,42 +9,43 @@ from tensorflow.keras import layers
 
 from sklearn.model_selection import train_test_split
 
-EPOCHS = 20
+MAX_EPOCHS = 51
+MAX_NODES = 3
+MAX_LAYERS = 2
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
-NUM_CATEGORIES = 43
+NUM_CATEGORIES = 3
 TEST_SIZE = 0.4
 
 
 def main():
 
     # Check command-line arguments
-    if len(sys.argv) not in [2, 3]:
-        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python traffic.py data_directory")
 
     # Get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
 
-    # Split data into training and testing sets
-    labels = tf.keras.utils.to_categorical(labels)
-    x_train, x_test, y_train, y_test = train_test_split(
-        np.array(images), np.array(labels), test_size=TEST_SIZE
-    )
+    for epochs in range(50, MAX_EPOCHS):
+        for layer in range(1, MAX_LAYERS):
+            for nodes in range(1, MAX_NODES):
+                # Split data into training and testing sets
+                labels = tf.keras.utils.to_categorical(labels)
+                x_train, x_test, y_train, y_test = train_test_split(
+                    np.array(images), np.array(labels), test_size=TEST_SIZE
+                )
 
-    # Get a compiled neural network
-    model = get_model()
+                # Get a compiled neural network
+                model = get_model(layer, nodes)
 
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
+                # Fit model on training data
+                model.fit(x_train, y_train, epochs=epochs)
 
-    # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
+                # Evaluate neural network performance
+                model.evaluate(x_test,  y_test, verbose=2)
 
-    # Save model to file
-    if len(sys.argv) == 3:
-        filename = sys.argv[2]
-        model.save(filename)
-        print(f"Model saved to {filename}.")
+                model.save(f"e{epochs}-l{layer}-n{nodes}.h5")
 
 
 def load_data(data_dir):
@@ -82,28 +83,24 @@ def load_data(data_dir):
     print(len(images), ":", len(labels))
     return images, labels
 
-def get_model():
+def get_model(layer_number, nodes):
     """
     Returns a compiled convolutional neural network model. Assume that the
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
 
-    model = keras.Sequential(
-        [
-            layers.Conv2D(5356, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
-            layers.MaxPooling2D(pool_size=(3, 3)),    
-            layers.Flatten(),
-            layers.Dense(700, activation="relu"),
-            layers.Dropout(.03),
-            layers.Dense(330, activation="relu"),
-            layers.Dense(456, activation="relu"),
-            layers.Dense(16, activation="relu"),
-            layers.Dense(NUM_CATEGORIES, activation="softmax")     
-        ]
-    )
-
-    model.build()
+    model = keras.Sequential([
+        layers.Conv2D(5356, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        layers.MaxPooling2D(pool_size=(3, 3)),
+        layers.Flatten()  
+    ])
+    for i in range(layer_number):
+        model.add(layers.Dense(nodes, activation="relu"))
+    
+    model.add(layers.Dense(NUM_CATEGORIES, activation="softmax"))
+    
+    model.build(input_shape=(layer_number, nodes))
 
     model.summary()
     
